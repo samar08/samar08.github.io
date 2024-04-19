425e29b9bd9514d7a33c7d7983d9ece63489e82d
samar08 <samarasimhareddy.mohan@gmail.com>
2024-04-19 23:21:28 +0530

    icons added

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -3,13 +3,20 @@ import '../styles/style.css'
 import { Avatar ,Button} from 'rsuite'
 import image from '../images/profilepic.jpg'
 import { Link } from 'react-router-dom'
+import { SocialIcon } from 'react-social-icons'
 const Sidebar = () => {
   return (
     <div className='sidebar col-sm-3'>
 
 
       <center><img src={image} className='rounded'/></center>
-      
+      <center><div style={{marginTop:'10px'}}>
+      <SocialIcon url="https://www.linkedin.com/in/samarasimha2/"/>
+      <SocialIcon url="https://github.com/samar08/"/>
+      {/* <SocialIcon url="https://www.hackerrank.com/profile/samarasimharedd2"/> */}
+      <SocialIcon url="https://www.facebook.com/Samar.simha.redde"/>
+      <SocialIcon url="https://www.instagram.com/a.mohan.samar/"/>
+      </div></center>
     <div className='sidebarbuttons'>
       <table className='table table-bordered text-center'>
         <tr>
