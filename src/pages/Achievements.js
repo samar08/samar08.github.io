9a9a8a9e7b1abb3113339456d37a2caaba44fb80
samar08 <samarasimhareddy.mohan@gmail.com>
2024-04-21 14:00:30 +0530

    Achievements

--- src/pages/Achievements.js
+++ src/pages/Achievements.js
@@ -1,10 +1,23 @@
 import React from 'react'
-
+import '../styles/style.css'
 const Achievements = () => {
   return (
     <div>
-      Achievements
-    </div>
+      <div className='box'>
+        <h1>Achievements</h1>
+      </div>
+      <div style={{marginLeft:"100px", marginTop:'20px'}}>
+        <p>1. Achieved <a href="https://www.hackerrank.com/certificates/bd636d909de2" target='_blank'>React(basic) certificate</a> from HackerRank in April 2024.</p>
+      <p> 2. Received <a href="https://drive.google.com/file/d/1SHDJnRJZ4lm_wilSidFIk8FNK9OBU2us/view?usp=sharing" target='_blank'>appreciation letter</a> from project lead at HPE in december 2022.</p>
+      <p>3. Achieved rank of <a href="https://drive.google.com/file/d/1ktynKGl7BzGpZzf6YN1fVFuC-f9aCzsY/view?usp=sharing" target='_blank'>1721 out of 10000+</a> participants in Google kickstart round c 2020.</p>
+      <p>4. Finalist in Techgig code gladiators 2020 out of 2 lakh participants.</p>
+      <p>5. Achieved <a href="https://www.hackerrank.com/certificates/ae944fd7a9c6" target='_blank'>Problem solving(Intermediate) certificate</a> from HackerRank in july 2020.</p>
+      <p>6. Achieved <a href="https://www.hackerrank.com/certificates/999627c700f1" target='_blank'>Problem solving(basic) certificate</a> from HackerRank in june 2020.</p>
+      <p>7. <a href="https://drive.google.com/file/d/1lNdca8CzIhySf8FBJnWoWccMiBCmXuSR/view?usp=sharing" target='_blank'>9th place out of 60 teams</a> in DEVSOC, a college hackathon conducted by codechef-vit in march 2019.</p>
+      <p>8. <a href="https://drive.google.com/file/d/1lOAl_YSKflF7MBnRa-RTSinkyNHLPWYY/view?usp=sharing" target='_blank'>Finalist</a> in Math-a-Thon conducted by department of mathematics vit in march 2019.</p>
+      </div>
+      </div>
+  
   )
 }
 
