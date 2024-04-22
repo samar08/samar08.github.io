4ee450531fc4d1b2498bb3ff6c0e0632d1108c38
samar08 <samarasimhareddy.mohan@gmail.com>
2024-04-22 16:04:44 +0530

    <center> tag

--- src/pages/Achievements.js
+++ src/pages/Achievements.js
@@ -3,10 +3,11 @@ import '../styles/style.css'
 const Achievements = () => {
   return (
     <div>
+      <center>
       <div className='box'>
         <h1>Achievements</h1>
       </div>
-      <div style={{marginLeft:"100px", marginTop:'20px', width:'80%'}}>
+      <div style={{ marginTop:'20px', width:'80%'}}>
         <p>1. Achieved <a href="https://www.hackerrank.com/certificates/bd636d909de2" target='_blank'>React(basic) certificate</a> from HackerRank in April 2024.</p>
       <p> 2. Received <a href="https://drive.google.com/file/d/1SHDJnRJZ4lm_wilSidFIk8FNK9OBU2us/view?usp=sharing" target='_blank'>appreciation letter</a> from project lead at HPE in december 2022.</p>
       <p>3. Achieved rank of <a href="https://drive.google.com/file/d/1ktynKGl7BzGpZzf6YN1fVFuC-f9aCzsY/view?usp=sharing" target='_blank'>1721 out of 10000+</a> participants in Google kickstart round c 2020.</p>
@@ -16,6 +17,7 @@ const Achievements = () => {
       <p>7. <a href="https://drive.google.com/file/d/1lNdca8CzIhySf8FBJnWoWccMiBCmXuSR/view?usp=sharing" target='_blank'>9th place out of 60 teams</a> in DEVSOC, a college hackathon conducted by codechef-vit in march 2019.</p>
       <p>8. <a href="https://drive.google.com/file/d/1lOAl_YSKflF7MBnRa-RTSinkyNHLPWYY/view?usp=sharing" target='_blank'>Finalist</a> in Math-a-Thon conducted by department of mathematics vit in march 2019.</p>
       </div>
+      </center>
       </div>
   
   )
