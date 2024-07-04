218517d593bef330c34fd906d71d2dc6e02fe26a
Samar <43211873+samar08@users.noreply.github.com>
2024-07-04 19:46:25 +0530

    Update Aboutme.js

--- src/pages/Aboutme.js
+++ src/pages/Aboutme.js
@@ -10,7 +10,7 @@ const Aboutme = () => {
       <div style={{ marginTop:'20px', width:'80%'}}>
        <p> I am a passionate software developer with two years of work experience at Hewlett Packard Enterprise(HPE). I did my bachelors in computer science and engineering with specialization in information security from vellore institute of technology, vellore, and graduated in the year 2021.
       I'm very much interested in web development with exposure to mobile development as well. I am very much interested in competitive coding and achieved good ranks in a few contests held worldwide. I prefer learning by doing and I believe that only practise makes a man perfect.
-      Thus I actively participated in a couple of hackathons to hone my skills and even stood in 9th place out of 60 teams in one of them.
+      Thus I actively participated in a couple of hackathons to hone my skills and even stood in 9th place out of 100 teams in one of them.
       
 
        </p>
